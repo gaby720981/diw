47c9440d4f828f593b5b9d11f346bf9c93ca70b3
Gaby <gaby_ag@live.com.ar>
2022-10-21 14:46:49 -0300

    ok

--- js/basico/script.js
+++ js/basico/script.js
@@ -1,15 +1,20 @@
+function nombre() {
+    nombre = prompt("Cuál es tu nombre?")
+    alert("¡Hola " + nombre + "!")
+}
 
+function edad() {
+    edad = prompt ("Bienvenido al Club Nocturno 'Xxxx'. Qué edad tienes?")
+    edad = parseInt (edad)
+    if (edad < 18) {
+        alert ("No podés pasar, lo siento...")
+    }
+    else {
+        alert ("Podés pasar. ¡Que disfrutes!")
+    }
+}
 
-//function mensaje() {
-//   alert("Holi")
-//}
+function mostrarMensaje() {
+    alert ("Bienvenido")
+}
 
-nombre = prompt("Cuál es tu nombre?")
-alert("Hola " + nombre + ", por eso ella no te ama xd")
-
-
-//edad = prompt ("Bienvenido al Club Nocturno 'Xxxx'. Qué edad tienes?")
-//edad = parseInt (edad)
-//if (edad < 18) {
-//    alert ("No podés pasar máquina...")
-//}
