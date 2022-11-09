55f5f8f0db5bef956c4bed355b15fad5a40f88ab
Gaby <gaby_ag@live.com.ar>
2022-11-09 14:50:47 -0300

    ok

---
+++ js/eventos/script.js
@@ -0,0 +1,9 @@
+function cambiarTitulo() {
+    const titulo = document.getElementById ("titulo_principal")
+    titulo.innerHTML = "¡Bienvenido!"
+}
+
+function cambiarTituloOriginal() {
+    const titulo2 = document.getElementById ("titulo_principal")
+    titulo2.innerHTML = "Vacaciones 2023"
+}
