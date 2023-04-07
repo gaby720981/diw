2c68d6b708d208707dacc6e6bc051c6788843d21
Gabriela Aguilera <gaby_ag@live.com.ar>
2023-04-07 15:23:13 -0300

    ok

--- js/practica/homero/myScript.js
+++ js/practica/homero/myScript.js
@@ -4,6 +4,6 @@
 function cambiarimg() {
     const img = document.getElementById('img') ;
     const src = img.src ;
-    const newSrc = src.endsWith('camaarriba.jpg') ? 'img/camaabajo.jpg' : 'img/camaarriba.jpg' ;
+    const newSrc = src.endsWith('camaarriba.png') ? 'img/camaabajo.png' : 'img/camaarriba.png' ;
     img.src = newSrc ;
 }
