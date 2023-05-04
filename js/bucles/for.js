d757b32dce08d3c4effb4c417cc86217d94d55e0
Gaby Aguilera <gaby_ag@live.com.ar>
2023-05-04 19:06:10 -0300

    ejer

---
+++ js/bucles/for.js
@@ -0,0 +1,9 @@
+// El bucle for inicia en la variable i con un valor de 200 y continúa mientras i sea menor o igual que 250.
+// En cada iteración del bucle, se agrega 2 a la variable i.
+// El número actual de la iteración se muestra en la consola usando console.log().
+
+
+for (let i = 200; i <= 250; i += 2) {
+    console.log(i);
+  }
+  
---
+++ js/bucles/while.js
@@ -0,0 +1,10 @@
+// Primero se declara una variable i con un valor de 200.
+// El bucle while continúa mientras i sea menor o igual que 250.
+// Dentro del bucle, el número actual de la iteración se muestra en la consola usando console.log().
+// Luego, se agrega 2 a la variable i antes de continuar con la siguiente iteración del bucle.
+
+let i = 200;
+while (i <= 250) {
+  console.log(i);
+  i += 2;
+}
